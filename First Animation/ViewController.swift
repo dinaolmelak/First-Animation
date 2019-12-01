59e530df439afafbfbdff1c7b94fdda7be4669b5
Dinaol Tadesse <dtadesse@iu.edu>
2019-12-01 02:16:36 -0600

    Initial Commit

---
+++ First Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Animation
+//
+//  Created by Dinaol Melak on 12/1/19.
+//  Copyright © 2019 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
