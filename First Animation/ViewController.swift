1b6117a2987f1304ed04500f00032830b931b7ae
Dinaol Tadesse <dtadesse@iu.edu>
2019-12-01 03:30:53 -0600

    Added Readme for my simple animation App

--- First Animation/ViewController.swift
+++ First Animation/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var helloLabel: UILabel!
     @IBOutlet weak var firstAnimationLabel: UILabel!
+    @IBOutlet weak var seeYouLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +20,37 @@ class ViewController: UIViewController {
         
         helloLabel.center.y -= view.bounds.height
         firstAnimationLabel.center.y += view.bounds.height
-        
+        seeYouLabel.alpha = 0.0
     }
     override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 1, animations: {
             self.helloLabel.center.y += self.view.bounds.height
-            self.firstAnimationLabel.center.y -= self.view.bounds.height
+
+        }) { (finished) in
+            self.secondAnimation()
         }
-        UIView.animate(withDuration: 2) {
+        UIView.animate(withDuration: 1, delay: 1.5, options: [], animations: {
             self.view.backgroundColor = #colorLiteral(red: 0.38846326, green: 0.6519199826, blue: 1, alpha: 1)
-        }
+        }, completion: {(finished) in
+            self.hiddenAnimation()
+        })
     }
 
+    
+    func secondAnimation() {
+        UIView.animate(withDuration: 1) {
+            self.firstAnimationLabel.center.y -= self.view.bounds.height
+        }
+    }
+    func hiddenAnimation() {
+        UIView.animate(withDuration: 2) {
+            self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        UIView.animate(withDuration: 1, delay: 1.5, options: [], animations: {
+            self.seeYouLabel.alpha = 1.0
+        }, completion: nil)
+    }
+    
 }
+    
 
---
+++ README.md
@@ -0,0 +1,6 @@
+#  First Animation
+
+##  This is a simple app I built to illustrate animation. 
+
+
+<img src= 'https://user-images.githubusercontent.com/32272045/69912085-4f2f5b00-13ea-11ea-8e78-7e4223e791cd.gif' width=250> <br>
