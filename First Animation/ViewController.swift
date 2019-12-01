d17fac3b496d69d9ad588c561773118be8c7f505
Dinaol Tadesse <dtadesse@iu.edu>
2019-12-01 02:31:16 -0600

    Initial State

--- First Animation/ViewController.swift
+++ First Animation/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var firstAnimationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
